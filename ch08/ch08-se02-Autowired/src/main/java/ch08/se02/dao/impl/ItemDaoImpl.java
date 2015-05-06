476e7a09d11d1e454cc0ff8513db71c3ed8beaed
heller <heller@H-L-PC>
2015-05-06 23:41:16 +0800

    @Qualifier注解配合@Autowired注解可以实现精确的自动装配。不过意义不大，因为完全可以直接由@Resource注解代替

--- ch08/ch08-se02-Autowired/src/main/java/ch08/se02/dao/impl/ItemDaoImpl.java
+++ ch08/ch08-se02-Autowired/src/main/java/ch08/se02/dao/impl/ItemDaoImpl.java
@@ -1,5 +1,7 @@
 package ch08.se02.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import ch08.se02.domain.Item;
@@ -7,4 +9,8 @@ import ch08.se02.domain.Item;
 @Repository("itemDao")
 public class ItemDaoImpl extends BaseDaoImpl<Item> {
 
+	@Autowired
+	@Qualifier("item")
+	private Item item;
+	
 }
--- ch08/ch08-se02-Autowired/src/main/java/ch08/se02/domain/Item.java
+++ ch08/ch08-se02-Autowired/src/main/java/ch08/se02/domain/Item.java
@@ -1,5 +1,8 @@
 package ch08.se02.domain;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Item {
 
 }
