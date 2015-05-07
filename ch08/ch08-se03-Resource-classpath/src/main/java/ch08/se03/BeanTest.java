eb9a150b550a293e25d8334d0a8e1ab53734358f
heller <heller@H-L-PC>
2015-05-08 01:26:02 +0800

    ch08-se03-Resource-classpath : classpath前缀可以用来匹配多个spring xml配置文件，它会合并配置为一个整个的ApplicationContext

--- ch08/ch08-se03-Resource-classpath/src/main/java/ch08/se03/BeanTest.java
+++ ch08/ch08-se03-Resource-classpath/src/main/java/ch08/se03/BeanTest.java
@@ -10,6 +10,7 @@ public class BeanTest {
 		// classpath前缀可以用来匹配多个spring xml配置文件，它会合并配置为一个整个的ApplicationContext
 		// classpath*:*-bean.xml表示匹配所有类加载路径下所有*-bean.xml形式的配置文件
 		// 而默认情况（不加通配符）只加载找到的第一个复合条件的xml配置
+		// t-bean.xml并不符合这个表达式，所以看到没有被加载
 		ctx = new ClassPathXmlApplicationContext("classpath*:*-bean.xml");
 		
 		String[] names = ctx.getBeanDefinitionNames();
