ca2336d25e41b8bae612e74a6826c420dc11ec66
heller <heller@H-L-PC>
2015-05-10 01:06:56 +0800

    ch08-se05-CacheEvict : 使用@CacheEvict注解可用于清除缓存

---
+++ ch08/ch08-se05-CacheEvict/src/main/java/ch08/se05/service/UserService.java
@@ -0,0 +1,15 @@
+package ch08.se05.service;
+
+import ch08.se05.domain.User;
+
+public interface UserService {
+
+	User getUserByNameAndAge(String name, int age);
+	
+	User getAnotherUser(String name, int age);
+
+	void evictUser(String name, int age);
+	
+	void evictAll();
+	
+}
