49b26b0d68ec56ae40c54ec00ba17239d1da7fa1
heller <heller@H-L-PC>
2015-05-23 23:21:12 +0800

    Hibernate test

---
+++ ch05/ch05-se06-Formula/src/main/java/ch05/se06/domain/News.java
@@ -0,0 +1,85 @@
+package ch05.se06.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Formula;
+
+@Entity
+@Table(name = "news_inf")
+public class News {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String title;
+	private String content;
+	
+	@Formula("(select concat(n.title, n.content) from news_inf n where n.id= id)")
+	private String fullContent;
+
+	public String getFullContent() {
+		return fullContent;
+	}
+	public void setFullContent(String fullContent) {
+		this.fullContent = fullContent;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		News other = (News) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", content=" + content
+				+ "]";
+	}
+
+}
---
+++ ch05/ch05-se06-Formula/src/test/java/ch05/se06/HibernateTest.java
@@ -0,0 +1,50 @@
+package ch05.se06;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import ch05.se06.domain.News;
+
+public class HibernateTest {
+	
+	@Test
+	public void testHibernate() {
+		//  实例化Configuration
+		Configuration cfg = new Configuration()
+			.configure();//configure()默认使用hibernate.cfg.xml，它可接受一个字符串参数，用于加载其他名字的配置文件
+		
+		// 获得SessionFactory
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		// 创建Session
+		Session session = sf.openSession();
+		// 开始事务
+		Transaction tx = session.beginTransaction();
+		
+		News n = new News();
+		n.setTitle("股市大涨！");
+		n.setContent("受国家战略驱动，股市出现爆发性增长，大批广场舞大妈入市，股市迎来新一轮的春天。");
+		
+		// 保存
+		session.save(n);
+		
+		// 实体现在处于持久化状态，所以现在已经有了id了
+		System.out.println(n.getId());
+		
+		// 提交事务
+		tx.commit();
+		
+		// 执行资源关闭操作
+		session.close();
+		
+		Session s = sf.openSession();
+		News nn = (News)s.get(News.class, n.getId());
+		System.out.println(nn.getFullContent());
+		
+		sf.close();
+	}
+
+}
